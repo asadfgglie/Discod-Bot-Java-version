57226a346dc968e1dbd9ea819c766fec0ad06a24
asadfgglie <ac121878@outlook.com>
2022-01-09 23:42:50 +0800

    Build app script 1.0

--- script/BuildApp.sh
+++ script/BuildApp.sh
@@ -2,17 +2,21 @@
 
 cd ..
 
-if [ -f "./build/libs/*.jar" ]; then
+Jar="./build/libs/"
+App="./App"
+
+if [ -e $Jar ]; then
   echo "Re-create App dictionary."
-  if [ -d "./App"]; then
+  if [ -e $App ]; then
     rm -rf ./App
   fi
+  mkdir ./App
 
   echo "Copy the file."
   cp ./build/libs/*.jar ./App/
-  ren App\*.jar DiscordBot-all.jar
+  mv ./App/*.jar ./App/DiscordBot-all.jar
 
-  cp ./resource/* ./App/
+  cp -r ./resource/* ./App/
 else
   echo "You need to run gradlew shadowJar before run this script."
 fi
