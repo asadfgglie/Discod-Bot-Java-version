b42800a63ed224ac33e3b0d95daebe3016fa47e4
asadfgglie <ac121878@outlook.com>
2022-01-21 23:45:04 +0800

    Main file 1.3 Add config update ability

--- src/main/java/ckcsc/asadfgglie/main/Basic.java
+++ src/main/java/ckcsc/asadfgglie/main/Basic.java
@@ -1,18 +1,18 @@
 package ckcsc.asadfgglie.main;
 
-import ckcsc.asadfgglie.main.services.Register.Register;
+import ckcsc.asadfgglie.main.json.JSONConfig;
 import ckcsc.asadfgglie.main.services.Register.ServiceArray;
 import ckcsc.asadfgglie.main.services.Register.Services;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.events.Event;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.InteractionHook;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
@@ -20,6 +20,7 @@ import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import net.dv8tion.jda.internal.entities.UserById;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -30,19 +31,17 @@ import java.util.EnumSet;
 import java.util.Iterator;
 import java.util.Objects;
 
-import static net.dv8tion.jda.api.interactions.commands.OptionType.INTEGER;
-
 public class Basic extends ListenerAdapter {
-    public static ArrayList<Register> REGISTER_LIST = new ArrayList<>();
+    public static ArrayList<Services> REGISTER_LIST = new ArrayList<>();
     public static ArrayList<User> ADMIN_USER_LIST = new ArrayList<>();
 
     public static ArrayList<CommandData> COMMAND_LIST = new ArrayList<>();
 
     public static String PATH;
 
-    public static JSONObject REGISTER_CONFIG;
-    public static JSONObject BOT_CONFIG;
-    public static JSONObject ADMIN_USER_CONFIG;
+    public static JSONConfig REGISTER_CONFIG;
+    public static JSONConfig BOT_CONFIG;
+    public static JSONConfig ADMIN_USER_CONFIG;
 
     public static JDA BUILDER;
 
@@ -73,11 +72,11 @@ public class Basic extends ListenerAdapter {
         ADMIN_USER_CONFIG = setConfig("AdminConfig.json", false);
     }
 
-    private static @NotNull JSONObject setConfig(String configName) throws IOException{
+    public static @NotNull JSONConfig setConfig(String configName) throws IOException{
         return Objects.requireNonNull(setConfig(configName, true));
     }
 
-    private static JSONObject setConfig(String configName, boolean compulsory) throws IOException {
+    public static @Nullable JSONConfig setConfig(String configName, boolean compulsory) throws IOException {
         BufferedReader configReader;
 
         if(compulsory) {
@@ -109,15 +108,15 @@ public class Basic extends ListenerAdapter {
 
         configReader.close();
 
-        return new JSONObject(configStr.toString());
+        return new JSONConfig(PATH + File.separator + configName, configStr.toString());
     }
 
     private static void setDefaultCommand(){
         COMMAND_LIST.add(
-            new CommandData("StopBot",
+            new CommandData("stop",
                             "Stop the bot.")
-                    .addOptions(new OptionData(INTEGER,
-                                                "IdOfBot",
+                    .addOptions(new OptionData(OptionType.STRING,
+                                                "id",
                                                 "The bot's ID",
                                                 true)));
     }
@@ -128,19 +127,23 @@ public class Basic extends ListenerAdapter {
 
             JSONObject serviceValues = REGISTER_CONFIG.getJSONObject(serviceClass);
 
-            if(Services.SERVICESLIST.containsKey(serviceClass)) {
+            if(Services.SERVICES_LIST.containsKey(serviceClass)) {
                 int serviceNumber = serviceValues.length();
 
-                ServiceArray serviceArray = Services.SERVICESLIST.get(serviceClass);
+                ServiceArray serviceArray = Services.SERVICES_LIST.get(serviceClass);
                 serviceArray.setArraySize(serviceNumber);
-                serviceArray.initializeArray();
+                serviceArray.initArray();
 
-                Iterator<String> serviceValueIterator = serviceValues.keySet().iterator();
+                Iterator<String> serviceValueIterator = serviceValues.keys();
                 for(int i = 0; serviceValueIterator.hasNext(); i++) {
                     String serviceName = serviceValueIterator.next();
+
                     Services service = serviceArray.array[i];
+
                     service.registerByEnvironment(serviceValues.getJSONObject(serviceName), serviceName);
+
                     REGISTER_LIST.add(service);
+
                     System.out.println("Success register service: " + service);
                 }
             }
@@ -177,11 +180,15 @@ public class Basic extends ListenerAdapter {
                     .enableCache(CacheFlag.VOICE_STATE)
                     .build();
 
+            for(Services s: REGISTER_LIST) {
+                BUILDER.addEventListener(s);
+            }
+
             // Discord slash Bot example code
             // These commands take up to an hour to be activated after creation/update/delete
             CommandListUpdateAction commands = BUILDER.updateCommands();
 
-            commands.addCommands(COMMAND_LIST);
+            commands.addCommands(COMMAND_LIST).queue();
 
             BUILDER.awaitReady();
         }
@@ -190,32 +197,49 @@ public class Basic extends ListenerAdapter {
         }
     }
 
-    @Override
-    public void onMessageReceived(@NotNull MessageReceivedEvent event){
-        callRegister(event);
+    public synchronized static void saveConfig(@NotNull JSONConfig config) {
+        BufferedWriter configWriter;
+        try {
+            configWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(config.fullConfigName)));
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            String configStr = config.toString();
+
+            configWriter.write(configStr, 0, configStr.length());
+
+            configWriter.flush();
+            configWriter.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event){}
+
+    @Override
     public void onSlashCommand(@Nonnull SlashCommandEvent event) {
+        System.out.println(event.getCommandString());
+
         if(!ADMIN_USER_LIST.contains(Objects.requireNonNull(event.getMember()).getUser())){
             InteractionHook hook = event.getHook();
             hook.sendMessage("Sorry, You are not an admin-user.").queue();
             return;
         }
-        if ("StopBot".equals(event.getName())) {
-            stopBot(Objects.requireNonNull(event.getOption("IdOfBot")));
+        if ("stop".equals(event.getName())) {
+            stopBot(Objects.requireNonNull(event.getOption("id")));
         }
     }
 
     private void stopBot(@NotNull OptionMapping idOfBot) {
-        if(BUILDER.getSelfUser().getIdLong() == idOfBot.getAsLong()){
+        if(BUILDER.getSelfUser().getId().equals(idOfBot.getAsString())){
             BUILDER.shutdown();
-        }
-    }
-
-    private void callRegister(Event event){
-        for (Register register: REGISTER_LIST){
-            register.call(event);
         }
     }
 }
