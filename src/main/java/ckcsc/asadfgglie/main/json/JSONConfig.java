b0f4d21615d4248a6ab233d99ab99ad56ae31741
asadfgglie <ac121878@outlook.com>
2022-01-27 12:02:08 +0800

    JSONConfig 1.2 add method toFormatString(String jsonStr)

--- src/main/java/ckcsc/asadfgglie/main/json/JSONConfig.java
+++ src/main/java/ckcsc/asadfgglie/main/json/JSONConfig.java
@@ -1,5 +1,6 @@
 package ckcsc.asadfgglie.main.json;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONObject;
 
 public class JSONConfig extends JSONObject {
@@ -10,85 +11,89 @@ public class JSONConfig extends JSONObject {
         this.fullConfigName = fullConfigName;
     }
 
-    // thanks for God timothychen write this
     @Override
-        public String toString(){
-            // get the original String
-            StringBuilder configStrBlr = new StringBuilder(super.toString());
+    public String toString(){
+        return toFormatString(super.toString());
+    }
 
-            String enterChar = ",{[}]";
-            int[] nowIndent = {0, 0};
-            int pointer = 0;
-            boolean isInString = false;
+    // thanks for God timothychen write this
+    public static @NotNull String toFormatString(String jsonStr){
+        // get the original String
+        StringBuilder configStrBlr = new StringBuilder(jsonStr);
 
-            // Iterate all String to add '\newLine\' in suitable place
-            for(; pointer < configStrBlr.length(); pointer++){
-                char i = configStrBlr.charAt(pointer);
+        String enterChar = ",{[}]";
+        int[] nowIndent = {0, 0};
+        int pointer = 0;
+        boolean isInString = false;
 
-                // check if in the String
-                if(i == '\'' || i == '\"'){
+        // Iterate all String to add '\newLine\' in suitable place
+        for(; pointer < configStrBlr.length(); pointer++){
+            char i = configStrBlr.charAt(pointer);
+
+            // check if in the String
+            if(i == '\'' || i == '\"'){
+                isInString = !isInString;
+            }
+
+            // add '\n' in front/back of the enterChar
+            String iStr = String.valueOf(i);
+            if (enterChar.substring(0, 3).contains(iStr) && !isInString) {
+                // in front
+                configStrBlr.insert(pointer + 1, "\\newLine\\");
+                pointer += 9;
+            } else if (enterChar.substring(3).contains(iStr) && !isInString) {
+                // in back
+                configStrBlr.insert(pointer, "\\newLine\\");
+                pointer += "\\newLine\\".length();
+            }
+        }
+
+        // split String
+        String[] configStrArray = configStrBlr.toString().split("\\\\newLine\\\\");
+
+        // add tab
+        for(int i = 0; i < configStrArray.length; i++){
+            // get String
+            StringBuilder eachLine = new StringBuilder(configStrArray[i]);
+            isInString = false;
+            pointer = 0;
+
+            for(; pointer < eachLine.length(); pointer++){
+                char j = eachLine.charAt(pointer);
+
+                if(j == '\'' || j == '\"'){
                     isInString = !isInString;
                 }
 
-                // add '\n' in front/back of the enterChar
-                String iStr = String.valueOf(i);
-                if (enterChar.substring(0, 3).contains(iStr) && !isInString) {
-                    // in front
-                    configStrBlr.insert(pointer + 1, "\\newLine\\");
-                    pointer += 9;
-                } else if (enterChar.substring(3).contains(iStr) && !isInString) {
-                    // in back
-                    configStrBlr.insert(pointer, "\\newLine\\");
-                    pointer += "\\newLine\\".length();
+                String iStr = String.valueOf(j);
+                if(enterChar.substring(1, 3).contains(iStr) && !isInString){
+                    // add tab
+                    nowIndent[1]++;
+                }
+                else if(enterChar.substring(3).contains(iStr) && !isInString){
+                    // reduce tab
+                    nowIndent[0]--;
+                    nowIndent[1]--;
                 }
             }
 
-            // split String
-            String[] configStrArray = configStrBlr.toString().split("\\\\newLine\\\\");
+            StringBuilder tab = new StringBuilder();
+            tab.append("\t".repeat(Math.max(nowIndent[0], 0)));
+            eachLine.insert(0, tab);
+            eachLine.append('\n');
 
-            // add tab
-            for(int i = 0; i < configStrArray.length; i++){
-                // get String
-                StringBuilder eachLine = new StringBuilder(configStrArray[i]);
-                isInString = false;
-                pointer = 0;
+            nowIndent[0] = nowIndent[1];
 
-                for(; pointer < eachLine.length(); pointer++){
-                    char j = eachLine.charAt(pointer);
+            // replace String
+            configStrArray[i] = eachLine.toString();
+        }
 
-                    if(j == '\'' || j == '\"'){
-                        isInString = !isInString;
-                    }
+        StringBuilder configStr = new StringBuilder();
 
-                    String iStr = String.valueOf(j);
-                    if(enterChar.substring(1, 3).contains(iStr) && !isInString){
-                        // add tab
-                        nowIndent[1]++;
-                    }
-                    else if(enterChar.substring(3).contains(iStr) && !isInString){
-                        // reduce tab
-                        nowIndent[0]--;
-                        nowIndent[1]--;
-                    }
-                }
-
-                StringBuilder tab = new StringBuilder();
-                tab.append("\t".repeat(Math.max(nowIndent[0], 0)));
-                eachLine.insert(0, tab);
-                eachLine.append('\n');
-
-                nowIndent[0] = nowIndent[1];
-
-                // replace String
-                configStrArray[i] = eachLine.toString();
-            }
-
-            StringBuilder configStr = new StringBuilder();
-
-            // connect whole String
-            for(String content: configStrArray){
-                configStr.append(content);
-            }
+        // connect whole String
+        for(String content: configStrArray){
+            configStr.append(content);
+        }
 
         return configStr.toString();
     }
