440e5118cbd1402a9e1cc5d1cfcb45bc087b924f
asadfgglie <ac121878@outlook.com>
2022-02-19 22:12:29 +0800

    Build app script 2.0

--- resource/buildscript.bat
+++ resource/buildscript.bat
@@ -1,3 +1,5 @@
+@ECHO off
+chcp 65001
 docker stop discordbot
 
 docker container rm discordbot
@@ -6,4 +8,4 @@ docker image rm discordbot:java11
 
 docker build -t discordbot:java11 ./ --no-cache
 
-docker run -d -v /home/asadfgglie/App/config:/bot/config --name discordbot discordbot:java11
+docker run -d -v ./config:/bot/config --name discordbot discordbot:java11
--- script/refreshConfig.json.cmd
+++
@@ -1,7 +0,0 @@
-@ECHO off
-chcp 65001
-
-cd ..
-
-if exist App\config\* del App\config\* /q
-xcopy resource\config\* App\config\* /E
