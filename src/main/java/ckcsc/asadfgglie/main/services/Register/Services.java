2b577054a984a1431e1f7e7e8d3637515ffd2424
asadfgglie <ac121878@outlook.com>
2022-02-19 22:31:02 +0800

    Service register system 1.2 add some initialize action register some new services

--- src/main/java/ckcsc/asadfgglie/main/services/Register/Services.java
+++ src/main/java/ckcsc/asadfgglie/main/services/Register/Services.java
@@ -1,23 +1,33 @@
 package ckcsc.asadfgglie.main.services.Register;
 
+import ckcsc.asadfgglie.main.Basic;
 import ckcsc.asadfgglie.main.services.GFloor;
+import ckcsc.asadfgglie.main.services.ai.AutoReply;
+import ckcsc.asadfgglie.main.services.ai.MNIST;
 import ckcsc.asadfgglie.main.services.MusicPlayer;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 
 public abstract class Services extends ListenerAdapter {
-    protected String serviceName = null;
-    protected long CHANNEL_ID = 0;
+    protected String serviceName;
+    protected Logger logger;
+    protected String description;
+    protected boolean isInfoVisible = true;
 
     public static HashMap<String, ServiceArray> SERVICES_LIST = new HashMap<>();
 
     public static void init() {
         loginService(GFloor.class.getSimpleName(), new GFloor());
         loginService(MusicPlayer.class.getSimpleName(), new MusicPlayer());
+        loginService(MNIST.class.getSimpleName(), new MNIST());
+        loginService(AutoReply.class.getSimpleName(), new AutoReply());
     }
 
     private static void loginService(String className, Services service){
@@ -26,33 +36,69 @@ public abstract class Services extends ListenerAdapter {
 
     protected Services(){}
 
-    public abstract void registerByEnvironment(JSONObject values, String name);
+    protected JSONObject getSelfConfig(){
+        return Basic.REGISTER_CONFIG.getJSONObject(this.getClass().getSimpleName()).getJSONObject(this.serviceName);
+    }
+
+    public abstract void registerByEnvironment(JSONObject values);
+
+    public void setLogger(String name){
+        this.logger = LoggerFactory.getLogger(this.getClass().getSimpleName() + " - " + name);
+    }
+
+    private void setServiceName(String name){
+        this.serviceName = name;
+    }
 
     public abstract Services copy();
 
     public void printlnInfo(String msg){
-        if (msg != null)
-            System.out.println(msg);
-        System.out.println(this + "\n");
+        if (msg != null) {
+            for(String str : msg.split("\n")) {
+                logger.info(str);
+            }
+        }
+        logger.info(this.toString());
+    }
+
+    private void setDescription(JSONObject values){
+        try {
+            this.description = values.getString("description");
+        }
+        catch (JSONException e){
+            this.logger.warn(this + " dose not have description.");
+        }
+    }
+
+    private void setInfoVisible (JSONObject values){
+        try {
+            this.isInfoVisible = values.getBoolean("isInfoVisible");
+        }
+        catch (JSONException e){
+            this.logger.warn("\"isInfoVisible\" doesn't set.");
+            this.logger.warn("\"isInfoVisible\" will be set true by default.");
+            this.logger.warn("If \"isInfoVisible\" set to false, other discord server member still con look this service description.");
+        }
+    }
+
+    public void basicSetting(JSONObject values, String name){
+        setServiceName(name);
+        setLogger(name);
+        setInfoVisible(values);
+        setDescription(values);
     }
 
     protected void printMsg(@NotNull MessageReceivedEvent event){
         if(event.isFromGuild()) {
-            System.out.println("<<" + event.getGuild().getName() + ">>" +
+            logger.info("<<" + event.getGuild().getName() + ">>" +
                     " #<" + event.getChannel().getName() + "> " +
-                    event.getAuthor().getName() + ":\n" +
-                    event.getMessage().getContentDisplay() + "\n");
-        }
-        else {
-            System.out.println("<<" + event.getChannel().getName() + ">>" +
                     event.getAuthor().getName() + ": " +
                     event.getMessage().getContentDisplay());
         }
-    }
-
-    protected void printlnErr(String msg){
-        if (msg != null)
-            System.err.println(msg);
-        System.err.println(this);
+        else {
+            logger.info("<<" + event.getChannel().getName() + ">>" +
+                    event.getAuthor().getName() + ": " +
+                    event.getMessage().getContentDisplay());
+        }
     }
 }
