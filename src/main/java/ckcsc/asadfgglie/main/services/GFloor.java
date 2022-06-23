d7f7b664a6141d4ff90c20e53ae6b59e7998fdd3
asadfgglie <ac121878@outlook.com>
2022-06-23 16:11:41 +0800

    GFloor service 1.4.3 fix update/delete message bug

--- src/main/java/ckcsc/asadfgglie/main/services/GFloor.java
+++ src/main/java/ckcsc/asadfgglie/main/services/GFloor.java
@@ -2,20 +2,29 @@ package ckcsc.asadfgglie.main.services;
 
 import ckcsc.asadfgglie.main.Basic;
 import ckcsc.asadfgglie.main.services.Register.Services;
-import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.entities.MessageType;
-import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.message.GenericMessageEvent;
+import net.dv8tion.jda.api.events.message.MessageDeleteEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
+import net.dv8tion.jda.internal.utils.tuple.Pair;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class GFloor extends Services {
     private long nowFloor = 0;
     private long maxFloor = 0;
-    private User lastFloorBreaker;
+    private @Nullable User lastFloorBreaker;
     private long lastFloorBuilderID = -1;
     private boolean isNeedPin = false;
+    /**
+     * < MessageIDLong, floor >
+     */
+    private final HashMap<Long, Long> floorHistory = new HashMap<>();
 
     private long CHANNEL_ID;
 
@@ -60,11 +69,11 @@ public class GFloor extends Services {
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event){
         if(event.getChannel().getIdLong() == CHANNEL_ID){
-            printMsg(event);
-
             MessageType messageType = event.getMessage().getType();
 
             if(messageType == MessageType.DEFAULT || messageType == MessageType.INLINE_REPLY) {
+                printMsg(event);
+
                 if (event.getAuthor() == event.getJDA().getSelfUser()) {
                     if(isNeedPin) {
                         event.getChannel().pinMessageById(event.getMessageId()).queue();
@@ -78,6 +87,49 @@ public class GFloor extends Services {
                     Basic.saveConfig(Basic.REGISTER_CONFIG);
                 }
             }
+        }
+    }
+
+    @Override
+    public void onMessageUpdate (@NotNull MessageUpdateEvent event) {
+        if(floorHistory.containsKey(event.getMessageIdLong())){
+            printMsg(event);
+            gUpdateCheck(event);
+        }
+    }
+
+    private void gUpdateCheck (@NotNull MessageUpdateEvent event) {
+        String message = event.getMessage().getContentDisplay();
+        String[] messageArray = message.split("\n");
+
+        String msg = messageArray[0];
+        msg = msg.split("\\s+")[0];
+
+        long floor;
+        try
+        {
+            floor = Long.parseLong(msg.split("g")[1]);
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            breakFloor(event, "樓層數字再亂改阿");
+            return;
+        }
+        if (!msg.matches("g\\d+")) {
+            breakFloor(event, "說好的g去哪裡了?");
+        }
+        if(floor != floorHistory.get(event.getMessageIdLong())){
+            breakFloor(event, "樓層數字再亂改阿");
+        }
+        printlnInfo(null);
+    }
+
+    @Override
+    public void onMessageDelete (@NotNull MessageDeleteEvent event) {
+        if(floorHistory.containsKey(event.getMessageIdLong())){
+            breakFloor(event, "偷偷拆樓的傢伙出來自首!");
+            printlnInfo("Floor was destroyed!");
         }
     }
 
@@ -111,23 +163,24 @@ public class GFloor extends Services {
         catch (Exception e)
         {
             System.out.println(e.getMessage());
-            breakFloor(event);
+            breakFloor(event, "樓層數字再亂打阿");
             return;
         }
 
         if (!msg.matches("g\\d+"))
         {
-            breakFloor(event);
+            breakFloor(event, "樓層數字再亂打阿");
         }
         else if (floor - nowFloor != 1)
         {
-            breakFloor(event);
+            breakFloor(event, "你不可以因為BNT第二劑副作用大，就說:\"我可不可以不打第二劑改打第三劑?\"\n\t\t同樣的，蓋房子也不能這樣蓋");
         }
         else if(lastFloorBuilderID == event.getAuthor().getIdLong()){
-            breakFloor(event);
+            breakFloor(event, "一個人不會連續死亡兩次，蓋G樓也是");
         }
         else
         {
+            floorHistory.put(event.getMessage().getIdLong(), floor);
             lastFloorBuilderID = event.getAuthor().getIdLong();
             nowFloor++;
 
@@ -139,26 +192,44 @@ public class GFloor extends Services {
         }
     }
 
-    private void breakFloor(@NotNull MessageReceivedEvent event){
-        lastFloorBreaker = event.getMessage().getAuthor();
+    private void breakFloor(@NotNull GenericMessageEvent e, String reason){
+        if(e instanceof MessageReceivedEvent) {
+            MessageReceivedEvent event = (MessageReceivedEvent) e;
+            lastFloorBreaker = event.getMessage().getAuthor();
+        }
+        else if(e instanceof MessageDeleteEvent){
+            lastFloorBreaker = null;
+        }
 
-        MessageChannel channel = event.getChannel();
-        channel.sendMessage(gMessage()).queue();
+        MessageChannel channel = e.getChannel();
+        channel.sendMessage(gMessage(reason)).queue();
 
         lastFloorBuilderID = -1;
         nowFloor = 0;
+        floorHistory.clear();
     }
 
-    private String gMessage(){
-        return "----------------------------\n" +
-               "上次紀錄：\t" + nowFloor + " 樓\n" +
-               "破壞者：\t\t" + lastFloorBreaker.getAsMention() + "\n" +
-               "最高紀錄：\t" + maxFloor + " 樓\n" +
-               "----------------------------";
+    private String gMessage(String reason){
+        if (lastFloorBreaker != null) {
+            return "----------------------------\n" +
+                   "上次紀錄：\t" + nowFloor + " 樓\n" +
+                   "破壞者：\t\t" + lastFloorBreaker.getAsMention() + "\n" +
+                   "最高紀錄：\t" + maxFloor + " 樓\n" +
+                   "原因：\t\t" + reason + "\n" +
+                   "----------------------------";
+        }
+        else{
+            return "----------------------------\n" +
+                    "上次紀錄：\t" + nowFloor + " 樓\n" +
+                    "最高紀錄：\t" + maxFloor + " 樓\n" +
+                    "原因：\t\t" + reason + "\n" +
+                    "----------------------------";
+        }
     }
 
     @Override
     public String toString(){
         return GFloor.class.getSimpleName() + "(serviceName: " + this.serviceName + ", CHANNEL_ID: " + this.CHANNEL_ID + ", nowFloor: " + this.nowFloor + ", maxFloor: " + this.maxFloor + ", lastFloorBuilder" + this.lastFloorBuilderID + ")";
     }
+
 }
